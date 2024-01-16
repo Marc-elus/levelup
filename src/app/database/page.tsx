428af5c963bfbd9677a7e215e2b3fa16a483f78e
Marcela <slobodaniuc1994@gmail.com>
2024-01-16 17:03:20 +0000

    levelup

--- src/app/database/page.tsx
+++ src/app/database/page.tsx
@@ -1,5 +1,11 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://CS8dvcbgGxAYUgyI:Sandra20112011@atlascluster.xoceqav.mongodb.net/?retryWrites=true&w=majority";
+import { MongoClient, ServerApiVersion } from 'mongodb'
+
+    
+const uri = process.env.MONGODB_URI
+if(!uri){
+    throw new Error("environment variable MONGODB_URI is not defined");
+}
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -8,13 +14,12 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server (optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // Querying our database
     const cursor = await client.db("test").collection("greetings").find();
     const array = await cursor.toArray()
     return array;
@@ -22,10 +27,11 @@ async function run() {
     // Ensures that the client will close when you finish/error
     await client.close();
   }
+
 }
 export default async function Database() {
-  const greetings =  await run();
-  return (<>
-      {greetings.map(greetingObj=> <h1>{greetingObj.greeting}</h1>)}
-  </>)
-}
+    const greetings =  await run();
+    return (<>
+        {greetings.map(greetingObj=> <h1 key={greetingObj._id.toString()}>{greetingObj.greeting}</h1>)}
+    </>)
+  }
